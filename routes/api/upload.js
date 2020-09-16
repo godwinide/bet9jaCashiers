05dbb40f0f8b22829baf9c959e6bbcdb782a261a
Godwin Idemudia <godwinide@gmail.com>
2020-09-16 08:34:41 +0100

    fixed some issues

--- routes/api/upload.js
+++ routes/api/upload.js
@@ -7,33 +7,31 @@ router.get("/", async (req,res) => {
     const cashier = await Cashier.findOne({cashierID});
     const {history} = cashier;
 
-    if(parseInt(cashier.tickets) < parseInt(tickets)){
-        const exists = history.some(h => h.id === id);
-        let newHistory;
-        // if it exits, update it
-        if(exists){
-            newHistory = history.map(h => {
-                if(h.id === id){
-                    h.sold = sold;
-                    h.tickets = tickets;
-                    h.wb = whiteBet;
-                }
-                return h;
-            });  
-        }else{
-            history.push({
-                sold,
-                tickets, 
-                wb: whiteBet,
-                date: new Date().toDateString(),
-                id: new Date().toDateString().replace(/\s/g, "").toLowerCase()
-            });
-    
-            newHistory = history;
-        }   
-            
-        await Cashier.updateOne({cashierID}, {sold, tickets, wb: whiteBet, history: newHistory});
-    }
+    const exists = history.some(h => h.id === id);
+    let newHistory;
+    // if it exits, update it
+    if(exists){
+        newHistory = history.map(h => {
+            if(h.id === id){
+                h.sold = sold;
+                h.tickets = tickets;
+                h.wb = whiteBet;
+            }
+            return h;
+        });  
+    }else{
+        history.push({
+            sold,
+            tickets, 
+            wb: whiteBet,
+            date: new Date().toDateString(),
+            id: new Date().toDateString().replace(/\s/g, "").toLowerCase()
+        });
+
+        newHistory = history;
+    }   
+        
+    await Cashier.updateOne({cashierID}, {sold, tickets, wb: whiteBet, history: newHistory});
 
     return res.json({success:true});
 })
