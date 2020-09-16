6722e9c9abaabdc10c2e823d3822c4add200de9a
Godwin Idemudia <godwinide@gmail.com>
2020-09-16 08:49:34 +0100

    fixed number issues

--- routes/api/upload.js
+++ routes/api/upload.js
@@ -7,33 +7,47 @@ router.get("/", async (req,res) => {
     const cashier = await Cashier.findOne({cashierID});
     const {history} = cashier;
 
-    const exists = history.some(h => h.id === id);
-    let newHistory;
-    // if it exits, update it
-    if(exists){
-        newHistory = history.map(h => {
-            if(h.id === id){
-                h.sold = sold;
-                h.tickets = tickets;
-                h.wb = whiteBet;
-            }
-            return h;
-        });  
-    }else{
-        history.push({
-            sold,
-            tickets, 
-            wb: whiteBet,
-            date: new Date().toDateString(),
-            id: new Date().toDateString().replace(/\s/g, "").toLowerCase()
-        });
+    async function updateCashier(){
+        const exists = history.some(h => h.id === id);
+        let newHistory;
+        // if it exits, update it
+        if(exists){
+            newHistory = history.map(h => {
+                if(h.id === id){
+                    h.sold = sold;
+                    h.tickets = tickets;
+                    h.wb = whiteBet;
+                }
+                return h;
+            });  
+        }else{
+            history.push({
+                sold,
+                tickets, 
+                wb: whiteBet,
+                date: new Date().toDateString(),
+                id: new Date().toDateString().replace(/\s/g, "").toLowerCase()
+            });
+    
+            newHistory = history;
+        }   
+            
+        await Cashier.updateOne({cashierID}, {sold, tickets, wb: whiteBet, history: newHistory});
+    }
 
-        newHistory = history;
-    }   
-        
-    await Cashier.updateOne({cashierID}, {sold, tickets, wb: whiteBet, history: newHistory});
+    if(new Date().getHours() >= 0 && new Date().getHours() <= 1){
+        await updateCashier();
+        return res.json({success:true});
+    }
+
+    else if(parseInt(cashier.tickets) <= parseInt(tickets)){
+        await updateCashier();
+        return res.json({success:true});
+    }
 
     return res.json({success:true});
+
+
 })
 
 module.exports = router;
